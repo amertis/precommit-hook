09b6755b78a4c8778a2bb27123fc47b126832b98
amertis <amertis@velti.com>
2016-06-08 11:26:05 +0300

    remove grep to work for windows

--- bin/eslintHook.js
+++ bin/eslintHook.js
@@ -1,7 +1,10 @@
-var pattern = 'git diff-index --name-only HEAD|grep "\\.js$"|grep -v "migrate*"'
+var pattern = 'git diff-index --name-only HEAD'
 var exec = require('child_process').exec;
 exec(pattern, function (error, stdout, stderr) {
     var files =  stdout.split('\n')
+    files = files.filter(function (f) {
+        return f.endsWith('.js') && !f.startsWith('migrate')
+    })
     files.pop()
     if(files.length == 0) {
         process.exit(0)
@@ -16,6 +19,7 @@ exec(pattern, function (error, stdout, stderr) {
     var report = engine.executeOnFiles(files);
     var hasErrors = report.errorCount > 0 || report.warningCount > 0;
     if (hasErrors) {
+        console.log(JSON.stringify(report))
         process.exit(1)
     }
 })
